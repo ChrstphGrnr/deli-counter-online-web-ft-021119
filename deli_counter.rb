449c55fd993b840727b80de9b71801b8dd677a51
Christoph Gruener <christophgruener@me.com>
2019-02-14 18:06:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(customer_array)
+  the_line = "The line is currently:"
+  if customer_array.length < 1 
+    the_line = "The line is currently empty."
+  else 
+    customer_array.each_with_index do |name, index|
+      the_line <<  " #{index+1}. #{name}"
+    end
+  end
+  puts the_line
+end
+
+
+    
+    
+
+    
+    
